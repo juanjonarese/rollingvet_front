4de0a2f796623f02e0630ff95f494ff7a8edf684
Juan Narese <juanjo.narese@gmail.com>
2025-08-07 18:35:49 -0300

    rutas protegidas y usestate navbar

--- src/routes/PrincipalRoutes.jsx
+++ src/routes/PrincipalRoutes.jsx
@@ -6,38 +6,48 @@ import ProductsScreen from "../pages/ProductsScreen";
 import OneProductScreen from "../pages/OneProductScreen";
 import ChangePassScreen from "../pages/ChangePassScreen";
 import AdminUsersScreen from "../pages/AdminUsersScreen";
-import AdminProducts from "../pages/AdminProductsScreen.jsx";
-// import AboutUsPage from "../pages/AboutUsPage";
 import Error404 from "../pages/error404.jsx";
 import FormularioPlanes from "../components/FormularioPlanes.jsx";
 import FooterApp from "../components/FooterApp.jsx";
+import AdminRoute from "./AdminRoutes.jsx";
 
 import LoginPage from "../pages/LoginPage";
 import SobreNosotros from "../pages/SobreNosotros";
+import AdminProductsScreen from "../pages/AdminProductsScreen.jsx";
 
 const PrincipalRoutes = () => {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<HomeScreen />} />
-        <Route path="/products" element={<ProductsScreen />} />
-        <Route path="/product/:id" element={<OneProductScreen />} />
-        <Route path="/aboutuspage" element={<SobreNosotros />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<RegisterScreen />} />
-        <Route path="/recoverymail" element={<RecoveryPassMailScreen />} />
-        <Route path="/changepass" element={<ChangePassScreen />} />
-        <Route path="/adminusers" element={<AdminUsersScreen />} />
-        <Route path="/error404" element={<Error404 />} />
-        <Route path="/footerapp" element={<FooterApp />} />
-        <Route path="/adminproducts" element={<AdminProducts />} />
-        <Route path="/FormularioPlanes" element={<FormularioPlanes />} />
+    <Routes>
+      <Route path="/" element={<HomeScreen />} />
+      <Route path="/products" element={<ProductsScreen />} />
+      <Route path="/product/:id" element={<OneProductScreen />} />
+      <Route path="/aboutuspage" element={<SobreNosotros />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<RegisterScreen />} />
+      <Route path="/recoverymail" element={<RecoveryPassMailScreen />} />
+      <Route path="/changepass" element={<ChangePassScreen />} />
+      <Route path="/error404" element={<Error404 />} />
+      <Route path="/footerapp" element={<FooterApp />} />
+      <Route path="/FormularioPlanes" element={<FormularioPlanes />} />
 
-        {/* <Route path="/about" element={<AboutScreen />} />
-        <Route path="/planes" element={<Planes />} />
-        <Route path="*" element={<NotFound />} /> */}
-      </Routes>
-    </>
+      {/* ✅ Rutas protegidas con wrapper */}
+      <Route
+        path="/admin/adminusers"
+        element={
+          <AdminRoute>
+            <AdminUsersScreen />
+          </AdminRoute>
+        }
+      />
+      <Route
+        path="/admin/adminproducts"
+        element={
+          <AdminRoute>
+            <AdminProductsScreen />
+          </AdminRoute>
+        }
+      />
+    </Routes>
   );
 };
 
