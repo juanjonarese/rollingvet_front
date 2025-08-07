22f4f97f13cc0c1966499255489692e4d813c227
Juan Narese <juanjo.narese@gmail.com>
2025-08-07 15:30:17 -0300

    bug css

--- src/pages/OneProductScreen.jsx
+++ src/pages/OneProductScreen.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import clientAxios from "../helpers/clientAxios";
-// import "../css/OneProduct.css";
+import "../css/unProducto.css";
 
 const OneProductScreen = () => {
   const { id } = useParams();
