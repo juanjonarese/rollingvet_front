e4c2f28088dfe299ea636a860dcba22cf70188b4
Juan Narese <juanjo.narese@gmail.com>
2025-08-07 14:41:01 -0300

    ruta de vercel

--- src/helpers/clientAxios.jsx
+++ src/helpers/clientAxios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const clientAxios = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://rollingvet-db.vercel.app/",
 });
 
 const token = JSON.parse(sessionStorage.getItem("token"));
